1c2300f9cb1c5c526494874fdec217d71e54b2ad
Ioana <goga_ioana@yahoo.com>
2014-08-03 22:57:41 +0200

    Renamed package

--- app/src/androidTest/java/com/example/android/sunshine/app/ApplicationTest.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.andorid.sunshine.app;
+package com.example.android.sunshine.app;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.andorid.sunshine.app;
+package com.example.android.sunshine.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
